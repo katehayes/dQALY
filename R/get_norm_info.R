cfce0c5d8c91b874e40237b8dea78d9abac30900
Kate <kate.hayes2@ukhsa.gov.uk>
2025-06-09 16:17:19 +0100

    add function default_norms which returns name of default for any country

--- R/get_norm_info.R
+++ R/get_norm_info.R
@@ -32,3 +32,33 @@ get_norm_info <- function(country = NULL, references = F) {
   norm_copy
 
 }
+
+
+#' Return default norms for a given country
+#'
+#' @param country A string - name of a permissible country
+#'
+#' @returns A string - the name of the default norm for the specified country
+#'
+#' @examples
+#' default_norms(country = "England")
+#' @export
+default_norms <- function(country) {
+
+  # we could also make it more complicated & allow user to choose between rules for selecting the default?
+  # eg most recent study, largest sample size, & so on
+
+  norm_copy <- copy(norm_info)
+
+  avail_countries <- unique(norm_copy$norm_country)
+
+  if(!(country %in% avail_countries)) {
+    stop(paste("Country not found. Countries for which utility norms are available currently include:", paste(avail_countries, collapse = ", ")))
+  } else {
+    default_norms <- norm_copy[norm_country == country & default == T]$norm_id
+  }
+
+  default_norms
+
+}
+
