16cba0a0018aa8d5bc56bbf5433d3a82cee810b8
Kate <kate.hayes2@ukhsa.gov.uk>
2025-06-11 15:34:11 +0100

    WORK IN PROGRESS thinking about validity checks - not implemented

--- R/calculate_dQALY.R
+++ R/calculate_dQALY.R
@@ -145,7 +145,13 @@ calculate_dQALY <- function(# we had discussed removing the default NULL from co
   # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 
-  if(.is_valid_r(r) == F) {
+  # Haven't figured this out yet - but want (if possible) to pass the environment env into this validity check function
+  # if(.is_valid_arg_combination() == FALSE) {
+  #   stop()
+  # }
+
+
+  if(.is_valid_r(r) == FALSE) {
     stop("Parameter r must be a numeric scalar between 0 and 1 or a function that specifies how the discount rate changes over time.
           See the README for examples of valid values for r.")
     # The function should take one argument (time point - number of years into the future) and return the desired discount rate at that time point.
@@ -175,21 +181,6 @@ calculate_dQALY <- function(# we had discussed removing the default NULL from co
   }
 
 
-
-  # if user doesn't want to group output:
-  # user can either specify country, year, norms (or just country and year if we set default norms for each country) OR
-  # user can specify country, year, and provide own norms OR
-  # user can specify country, norms, and provide life tables OR
-  # (OR - if we set default utility norms for each country - user can provide own life table and specify country?)
-  # OR user can provide own life table and utility norms
-
-
-  # if user is providing own life table and utility norms but wants to group output:
-  # either they need to provide their own cohort
-  # OR they also need to specify country and year, in order to get the default population norm
-
-  # (probably wouldn't make a lot of sense methodologically to provide your own custom life tables
-  # and then use a default country-level population weighting to group)
 
 
   # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
@@ -460,7 +451,7 @@ calculate_QALE <- function(...) {
 
 
 # ------------------------------------------------------------------------- #
-# -------------------------------- INTERNALS ------------------------------ #
+# ---------------- INTERNALS - VALIDITY CHECKING -------------------------- #
 # ------------------------------------------------------------------------- #
 
 
@@ -497,7 +488,116 @@ calculate_QALE <- function(...) {
 }
 
 
-#
+# --------Checking for appropriate combinations of arguments -----------------------------------------------------
+
+
+
+.is_valid_arg_combination <- function() {
+
+  # check for appropriate combination of parameters:
+  # User can interact with function in a number of ways
+  # 1. rely entirely on package data
+  # 2. supply all their own data - life tables, utility norms & (if needed bc user is grouping) cohort
+  # 3. use mix of package & own data
+  # As a result there are a few different sets of possible combinations of values for arguments
+
+  # (note: making a series of separate if statements rather than if else's,
+  # just for my own ease of understanding, but could change this?)
+
+  # 1. in the case when the user is relying on package life tables
+  if(.is_user_supplied(life_table) == FALSE) {
+
+    # The user must specify a country and a year
+    if(is.null(country) == TRUE | is.null(year) == TRUE) {
+      warning("Please specify values for both of the arguments 'country' and 'year'.")
+      return(FALSE)
+    }
+
+  }
+
+  # 2. in the case when the user is supplying their own life tables
+  if(.is_user_supplied(life_table) == TRUE) {
+
+    # RULE: if the user would like to group output, they need to supply their own cohort too
+    # (this could change if it isn't sensible - my the logic for this choice is that it doesn't
+    # make a lot of sense to me methodologically to provide your own custom life tables
+    # and then use a default country-level population weighting to group, since saying that survival is
+    # significantly different from the country-level average in the cohort you're trying to study is
+    # equivalent to saying that the cohort structure is significantly different)
+    if(.will_group(collapse_age, collapse_sex) == TRUE) {
+      if(is.null(cohort)) {
+        warning("If you are supplying your own custom life tables to the function, and you would
+                like to group the function output, then you must also supply your own custom cohort
+                to the function.")
+        return(FALSE)
+      }
+    }
+
+  }
+
+
+
+  # 2.1 in the case when the user is supplying their own life tables but NOT supplying their own utility norms (ie using package utility norms)
+  if(.is_user_supplied(life_table) == TRUE & .is_user_supplied(norms) == FALSE) {
+
+    # the user doesn't have to specify YEAR but must still specify COUNTRY
+    if(is.null(country) == TRUE) {
+      # Very much need to return to re-write warning
+      # This check/this error message might change subject to whether we allow users to select a norm from ANY country
+      # using norm_ids (would have to revise norm_ids so they are all unique - not just as they are presently which is
+      # unique within countries)
+      # (note to self - another option - probably more complicated?, would be to have a function that allows users to
+      # return instances of the actual norm data the package stores, and then they could use this data as input to the
+      # calculation, and we'd treat it as a user-supplied custom set of norms??)
+      warning("No custom utility norms have been supplied to the function.
+              That means an appropriate set of utility norms stored in package data must be selected for use in the calculation.
+              Please indicate which country you intend to produce estimates for, by specifying a value for the argument 'country',
+              so that the function can make an appropriate selection.")
+      # PERHAPS ADD: "or evaluate the appropriateness of your selection."
+      return(FALSE)
+    }
+  }
+
+
+  # 2.2 in the case when the user is supplying their own life tables and utility norms
+  if(.is_user_supplied(norms) == TRUE & .is_user_supplied(life_table) == TRUE) {
+
+    # if the user also supplied country and year, don't stop the function but
+    # do let them know that the values they chose are irrelevant/are not being used to produce estimates
+    if(!is.null(country) | !is.null(year)) {
+      warning("If you are supplying your own custom life tables AND custom utility norms to the function,
+              then you do not have to supply a value for arguments 'country' and 'year, and any values you
+              do supply for these arguments are ignored - i.e. they do not get used by the function at any
+              point in the calculation of its outputs. ")
+    }
+  }
+
+}
+
+
+
+.is_user_supplied <- function(argument) {
+  if(length(argument) > 1L) {
+    TRUE
+  } else {
+    FALSE
+  }
+}
+
+.will_group <- function(collapse_age, collapse_sex) {
+  if(length(collapse_age) > 1L) {
+    TRUE
+  } else if(collapse_age == TRUE | collapse_sex == TRUE) {
+    TRUE
+  } else {
+    FALSE
+  }
+}
+
+
+# --------Checks at the single argument level----------------------------------------------------------
+
+
 # .are_valid_norms <- function(norms) {
 #   if (is.character(norms) && length(norms) == 1L && !is.na(norms)) {
 #     norm_copy <- copy(norm_info)
@@ -528,3 +628,291 @@ calculate_QALE <- function(...) {
 #
 # }
 
+
+
+# --------Checks at the single argument level, specifically when user is supplying own data----------------------------------------------------------
+
+.are_valid_custom_norms <- function(norms) {
+  # check if there is the right number of columns
+  if(length(norms) != 4L) {
+    warning("User-supplied utility norms are not in correct form.
+             Utility norms need to be list-like object with four columns, with names 'age_low', 'age_high', 'sex', 'avg_util'.")
+    return(FALSE)
+
+    # given there is the right number, check that the columns have the right names
+  } else if(any(!colnames(norms) %in% c("age_low", "age_high", "sex", "avg_util"))) {
+    warning("User-supplied life tables are not in correct form.
+             Columns must have names age_low', 'age_high', 'sex', 'avg_util'.")
+    return(FALSE)
+
+    # given they have the right names, check that the cols have the right type of values
+  } else if(.is_valid_sex_col(norms$sex) == FALSE) {
+    return(FALSE)
+  } else if(.is_valid_age_low_col(norms$age_low) == FALSE) {
+    return(FALSE)
+  } else if(.is_valid_age_high_col(norms$age_high) == FALSE) {
+    return(FALSE)
+  } else if(.is_valid_avg_util_col(norms$avg_util) == FALSE) {
+    return(FALSE)
+
+    # given the cols have appropriate values individually, check that they relate appropriately to each other
+  } else if(sum(norms$age_low > norms$age_high) > 0) {
+    warning("For user-supplied utility norm, the Value in column 'age_low' must always
+            be lower than or equal to the corresponding value in column 'age_high'.")
+    return(FALSE)
+  } else {
+
+    # all checks are satisfied
+    TRUE
+  }
+
+}
+
+
+.is_valid_custom_cohort <- function(cohort) {
+  # check if there is the right number of columns
+  if(length(cohort) != 3L) {
+    warning("User-supplied cohort is not in correct form.
+             Cohort need to be list-like object with three columns, with names 'x', 'sex', 'count'.")
+    return(FALSE)
+
+    # given there is the right number, check that the columns have the right names
+  } else if(any(!colnames(cohort) %in% c("x", "sex", "cohort"))) {
+    warning("User-supplied life tables are not in correct form.
+             Columns must have names 'x', 'sex', 'count'.")
+    return(FALSE)
+
+    # given they have the right names, check that the cols have the right type of values
+  } else if(.is_valid_sex_col(cohort$sex) == FALSE) {
+    return(FALSE)
+  } else if(.is_valid_cohort_x_col(cohort$x) == FALSE) {
+    return(FALSE)
+  } else if(.is_valid_count_col(cohort$count) == FALSE) {
+    return(FALSE)
+  } else {
+
+    # all checks are satisfied
+    TRUE
+  }
+
+}
+
+.is_valid_custom_lt <- function(life_table) {
+
+  # check if there is the right number of columns
+  if(length(life_table) != 3L) {
+    warning("User-supplied life tables are not in correct form.
+             Life tables need to be list-like object with three columns, with names 'sex', 'x', 'q_x'.")
+    return(FALSE)
+
+  # given there is the right number, check that the columns have the right names
+  } else if(any(!colnames(life_table) %in% c("sex", "x", "q_x"))) {
+    warning("User-supplied life tables are not in correct form.
+             Columns must have names 'sex', 'x', 'q_x'.")
+    return(FALSE)
+
+  # given they have the right names, check that the cols have the right type of values
+  } else if(.is_valid_sex_col(life_table$sex) == FALSE) {
+    return(FALSE)
+  } else if(.is_valid_lt_x_col(life_table$x) == FALSE) {
+    return(FALSE)
+  } else if(.is_valid_qx_col(life_table$q_x) == FALSE) {
+    return(FALSE)
+  } else {
+  # all checks are satisfied
+    TRUE
+  }
+
+}
+
+
+.are_valid_custom_age_groups <- function(age_groups) {
+
+  # check if there is the right number of columns
+  if(length(age_groups) != 2L) {
+    warning("User-supplied age_groups are not in correct form.
+             Age groups need to be list-like object with two columns, with names 'age_low', 'age_high'.")
+    return(FALSE)
+
+    # given there is the right number, check that the columns have the right names
+  } else if(any(!colnames(age_groups) %in% c("age_low", "age_high"))) {
+    warning("User-supplied age groups are not in correct form.
+             Columns must have names 'age_low', 'age_high'.")
+    return(FALSE)
+
+    # given they have the right names, check that the cols have the right type of values
+  } else if(.is_valid_age_group_col(age_groups$age_low) == FALSE) {
+    return(FALSE)
+  } else if(.is_valid_age_group_col(age_groups$age_high) == FALSE) {
+    return(FALSE)
+
+  # given the cols have appropriate values individually, check that they relate appropriately to each other
+  } else if(sum(age_groups$age_low > age_groups$age_high) > 0) {
+    warning("For user-supplied age groups, the Value in column 'age_low' must always
+            be lower than or equal to the corresponding value in column 'age_high'.")
+    return(FALSE)
+  } else {
+    # all checks are satisfied
+    TRUE
+  }
+
+}
+
+
+
+
+
+
+# --------Column level checks (for user-supplied data)-----------------------------------------------------------
+
+.is_valid_sex_col <- function(sex_col) {
+  if(any(!(sex_col %in% c("male", "female")))) {
+    # check that only values in column are either male or female
+    warning("'sex' column should only take values \"male\" or \"female\".")
+    return(FALSE)
+  } else if(sum(sex_col == "male") != sum(sex_col == "female")) {
+    # check whether there are same number of males as females
+    # I think the function should be able to handle it if there aren't (will check this),
+    # but we can warn the user that it's not what we expect
+    # NOTE: i need to come back to this warning message and re-write so its clear
+    warning("User-supplied data does not have same number of values for sex = female and sex = male.")
+    return(TRUE)
+  } else {
+    TRUE
+  }
+}
+
+
+.is_valid_qx_col <- function(qx_col) {
+  if(any(!is.numeric(qx_col))) {
+    stop("'q_x' column must contain only numbers between 0 and 1.")
+    return(FALSE)
+  } else if(any(qx_col < 0) | any(qx_col > 1)) {
+    warning("'q_x' column must contain only numbers between 0 and 1.")
+    return(FALSE)
+  } else {
+    TRUE
+  }
+}
+
+
+.is_valid_avg_util_col <- function(avg_util_col) {
+  if(any(!is.numeric(avg_util_col))) {
+    stop("'avg_util' column must contain only numbers between 0 and 1.")
+    return(FALSE)
+  } else if(any(avg_util_col < 0) | any(avg_util_col > 1)) {
+    warning("'avg_util' column must contain only numbers between 0 and 1.")
+    return(FALSE)
+  } else {
+    TRUE
+  }
+}
+
+# need different things from the x column in a user-supplied life table and a user-supplied cohort
+# not sure about this check yet - not finished
+.is_valid_lt_x_col <- function(x_col) {
+  # 1. needs to be all natural numbers
+  # 2. needs to start at 0 and go to at least....99?
+  # 3. needs to have no missing numbers between lowest and highest
+  # NEED TO WRITE CHECK FOR THIRD POINT
+  if(any(!is.numeric(x_col))) {
+    stop("'x' column in user-supplied life tables must contain only positive integers.")
+    return(FALSE)
+  } else if(any(x_col < 0) | any(x_col %% 1 != 0)) {
+    warning("'x' column in user-supplied life tables must contain only positive integers.")
+    return(FALSE)
+  } else if(max(x_col) < 99 | min(x_col) != 0) {
+    warning("User-supplied life tables must contain data for all ages from 0 up to and including 99.")
+    return(FALSE)
+  } else {
+    TRUE
+  }
+}
+
+
+# need different things from the x column in a user-supplied life table and a user-supplied cohort
+.is_valid_cohort_x_col <- function(x_col) {
+  # 1. needs to be all natural numbers
+  if(any(!is.numeric(x_col))) {
+    stop("'x' column in user-supplied cohort must contain only positive integers.")
+    return(FALSE)
+  } else if(any(x_col < 0) | any(x_col %% 1 != 0)) {
+    warning("'x' column in user-supplied cohort must contain only positive integers.")
+    return(FALSE)
+  } else {
+    TRUE
+  }
+}
+
+.is_valid_age_low_col <- function(age_low_col) {
+  # 1. needs to be all natural numbers
+  # 2. needs to start at 0
+  if(any(!is.numeric(age_low_col))) {
+    stop("'age_low' column must contain only positive integers.")
+    return(FALSE)
+  } else if(any(age_low_col < 0) | any(age_low_col %% 1 != 0)) {
+    warning("'age_low' column must contain only positive integers.")
+    return(FALSE)
+  } else if(min(age_low_col) != 0) {
+    # 99 is placeholder - need to figure out overall approach for dealing with low & high ages throughout package
+    warning("User-supplied utility norms must include values for age groups spanning from age 0 until at least 99 inclusive.")
+    return(FALSE)
+  } else {
+    TRUE
+  }
+
+}
+
+
+.is_valid_age_high_col <- function(age_high_col) {
+  # 1. needs to be all natural numbers
+  # 2. needs to end at min 99 (placeholder)
+  if(any(!is.numeric(age_high_col))) {
+    stop("'age_high' column must contain only positive integers.")
+    return(FALSE)
+  } else if(any(age_high_col < 0) | any(age_high_col %% 1 != 0)) {
+    warning("'age_high' column must contain only positive integers.")
+    return(FALSE)
+  } else if(max(age_high_col) < 99) {
+    # 99 is placeholder - need to figure out overall approach for dealing with low & high ages throughout package
+    warning("User-supplied utility norms must include values for age groups spanning from age 0 until at least 99 inclusive.")
+    return(FALSE)
+  } else {
+    TRUE
+  }
+
+}
+
+.is_valid_age_group_col <- function(age_group_col) {
+  # 1. needs to be all natural numbers
+  if(any(!is.numeric(age_group_col))) {
+    stop("In user-supplied age groups, 'age_low' and 'age_high' columns must contain only positive integers.")
+    return(FALSE)
+  } else if(any(age_group_col < 0) | any(age_group_col %% 1 != 0)) {
+    warning("In user-supplied age groups, 'age_low' and 'age_high' columns must contain only positive integers.")
+    return(FALSE)
+  } else {
+    TRUE
+  }
+}
+
+.is_valid_count_col <- function(count_col) {
+  # 1. needs to be numbers >= 0
+  if(any(!is.numeric(count_col))) {
+    stop("'count' column must contain only positive integers.")
+    return(FALSE)
+  } else if(any(count_col < 0)) {
+    warning("'count' column must contain only positive integers.")
+    return(FALSE)
+  } else {
+    TRUE
+  }
+
+}
+
+
+# if(.is_valid_arg_combination(country = get("country", env), year = get("year", env),
+#                              life_table = get("life_table", env), norms  = get("norms", env),
+#                              collapse_age = get("collapse_age", env), collapse_sex = get("collapse_sex", env)) == FALSE) {
+#   stop()
+# }
