69204234b7a5a9608a89aac3eb0c8df8cd9047d3
Kate <kate.hayes2@ukhsa.gov.uk>
2025-07-07 13:20:11 +0100

    fixing issue - making sure internal data is saved as data tables, as previously

--- data-raw/save_data.R
+++ data-raw/save_data.R
@@ -4,10 +4,10 @@
 
 root <- file.path(here::here(), "data-raw")
 
-utility_norms <- read.csv(file.path(root, "utility_norms.csv"), row.names = 1L)
-norm_info <- read.csv(file.path(root, "norm_info.csv"), row.names = 1L)
-life_tables <- read.csv(file.path(root, "life_tables.csv"), row.names = 1L)
-populations <- read.csv(file.path(root, "populations.csv"), row.names = 1L)
+utility_norms <- as.data.table(read.csv(file.path(root, "utility_norms.csv"), row.names = 1L))
+norm_info <- as.data.table(read.csv(file.path(root, "norm_info.csv"), row.names = 1L))
+life_tables <- as.data.table(read.csv(file.path(root, "life_tables.csv"), row.names = 1L))
+populations <- as.data.table(read.csv(file.path(root, "populations.csv"), row.names = 1L))
 
 
 # Build sysdata.rda
